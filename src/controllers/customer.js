63842257c1c0d0368eeb52d1c813286d85c31a1b
AdelinoJonas <84252481+AdelinoJonas@users.noreply.github.com>
2022-04-30 18:04:38 -0300

    cadastrar cliente

--- src/controllers/customer.js
+++ src/controllers/customer.js
@@ -50,6 +50,7 @@ const registerCustomer = async (req, res) => {
         return res.status(200).json({
             "mensagem": "Cliente cadastrado com sucesso"
         });
+        
     } catch (error) {
         return res.status(400).json(error);
     }
