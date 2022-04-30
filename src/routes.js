992711cc4d8e10e381404e115892e562178d8140
AdelinoJonas <84252481+AdelinoJonas@users.noreply.github.com>
2022-04-30 19:05:23 -0300

    componentização

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const customer = require('./controllers/customer');
+const customer = require('./controllers/customer/registerCustomer');
 
 const routes = express();
 
