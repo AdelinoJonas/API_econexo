6e74202f2b8b7d1ac065de63f60a6402c8c76b17
Wanderson C. Serra <wanderson.net@outlook.com.br>
2022-05-01 02:06:04 -0300

    DB Rotas e CRUD

--- src/connection.js
+++ src/connection.js
@@ -1,18 +1,21 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
+//const dotenv = require('dotenv'); - Apenas local (testes)
+
+//dotenv.config(); - Apenas local (testes)
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'api_econexo_db',
-    password: '123456',
-    port: 5432
+  user: "postgres",
+  host: "localhost",
+  database: "api_econexo_db",
+  password: "123456",
+  port: 5432,
 });
 
 const query = (text, param) => {
-    return pool.query(text, param);
-}
+  return pool.query(text, param);
+};
 
 module.exports = {
-    query,
-    pool
-}
+  query,
+  pool,
+};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const customer = require('./controllers/customer');
+const express = require("express");
+const customer = require("./controllers/customer");
+const product = require("./controllers/product");
 
 const routes = express();
 
-routes.post('/cadastrarCliente', customer.registerCustomer);
+routes.post("/cadastrarCliente", customer.registerCustomer);
+routes.post("/cadastrarProduto", product.createProduct);
 
 module.exports = routes;
