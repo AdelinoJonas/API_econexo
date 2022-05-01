63114c87e4179d427fc6f922b8473bdf518fca67
Wanderson C. Serra <wanderson.net@outlook.com.br>
2022-05-01 19:02:51 -0300

    Correção rotas produtos

--- src/routes.js
+++ src/routes.js
@@ -8,19 +8,19 @@ routes.post("/cadastrarCliente", customer.registerCustomer);
 // ==> Definindo as rotas do CRUD - 'Product':
 
 // ==> Rota responsável por criar um novo 'Product': (POST): localhost:3000/api/products
-router.post("/products", product.createProduct);
+routes.post("/products", product.createProduct);
 
 // ==> Rota responsável por listar todos os 'Products': (GET): localhost:3000/api/products
-router.get("/products", product.listAllProducts);
+routes.get("/products", product.listAllProducts);
 
 // ==> Rota responsável por selecionar 'Product' pelo 'Id': (GET): localhost:3000/api/products/:id
-router.get("/products/:id", product.findProductById);
+routes.get("/products/:id", product.findProductById);
 
 // ==> Rota responsável por atualizar 'Product' pelo 'Id': (PUT): localhost: 3000/api/products/:id
-router.put("/products/:id", product.updateProductById);
+routes.put("/products/:id", product.updateProductById);
 
 // ==> Rota responsável por excluir 'Product' pelo 'Id': (DELETE): localhost:3000/api/products/:id
-router.delete("/products/:id", product.deleteProductById);
+routes.delete("/products/:id", product.deleteProductById);
 
 // => Exportação do módulo
 module.exports = routes;
